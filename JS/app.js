281898c8d1412646b042d15795f4d9f9c9a80bc5
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-05-17 18:17:05 -0500

    introtohtml file created

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,12 @@
 // skewed hero triangles ===================
-const TriangleOne = document.querySelector('.triangleOne');
-const TriangleTwo = document.querySelector('.triangleTwo');
+const triangleOne = document.querySelector('.triangleOne');
+const triangleTwo = document.querySelector('.triangleTwo');
 
 window.addEventListener('scroll', function() {
     const value1 = -15 + window.scrollY / 45;
     const value2 = 15 + window.scrollY / -45;
-    TriangleOne.style.transform = "skewY(" + value1 + "deg)";
-    TriangleTwo.style.transform = "skewY(" + value2 + "deg)";
+    triangleOne.style.transform = "skewY(" + value1 + "deg)";
+    triangleTwo.style.transform = "skewY(" + value2 + "deg)";
 })
 
 // end of skewed hero triangles ===================
