2f9fd5c408518cefa64ee7a472c4b8f841dd395c
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-05-24 06:07:37 -0500

    develop commit for practica.html

---
+++ JS/practica.js
@@ -0,0 +1,33 @@
+function compile() {
+  const html = document.getElementById("code");
+  const code = document.getElementById("display").contentWindow.document;
+
+  document.body.onkeyup = function () {
+    code.open();
+    code.writeln(
+      html.value + "<style>" + "</style>" + "<script>" + "</script>"
+    );
+    code.close();
+  };
+}
+
+compile();
+
+const left = document.getElementById("drag-left");
+const right = document.getElementById("drag-right");
+const bar = document.getElementById("dragbar");
+
+const drag = function (e) {
+  document.selection
+    ? document.selection.empty()
+    : window.getSelection().removeAllRanges();
+  left.style.width = e.pageX - bar.offsetWidth / 2 + "px";
+};
+
+bar.addEventListener("mousedown", function () {
+  document.addEventListener("mousemove", drag);
+});
+
+bar.addEventListener("mouseup", function () {
+  document.removeEventListener("mousemove", drag);
+});
